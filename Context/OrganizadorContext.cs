09edc5319f7ed11dabfbc411faa5146b5aaf90b0
Jose Ailton <joseailton.ailtontinho@gmail.com>
2025-08-31 21:09:50 -0300

    Versão final do projeto com CRUD de tarefas

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -11,5 +11,42 @@ namespace TrilhaApiDesafio.Context
         }
 
         public DbSet<Tarefa> Tarefas { get; set; }
+
+        // Adicione este método para configurar a chave primária
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>()
+                .HasKey(t => t.Id); // Define Id como Chave Primária
+            
+            modelBuilder.Entity<Tarefa>()
+                .Property(t => t.Id)
+                .ValueGeneratedOnAdd(); // Configura o Id para ser auto-incrementado
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// using Microsoft.EntityFrameworkCore;
+// using TrilhaApiDesafio.Models;
+
+// namespace TrilhaApiDesafio.Context
+// {
+//     public class OrganizadorContext : DbContext
+//     {
+//         public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+//         {
+            
+//         }
+
+//         public DbSet<Tarefa> Tarefas { get; set; }
+//     }
+// }
